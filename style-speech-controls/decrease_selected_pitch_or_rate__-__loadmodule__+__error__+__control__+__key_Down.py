6694f6e8a8eb92b1e837024a52ea58a0fd7c2e84
José Vilmar Estácio de Souza <vilmar@informal.com.br>
2019-09-19 20:32:16 -0300

    Close file /tmp/orca-ratechanger.

--- style-speech-controls/decrease_selected_pitch_or_rate__-__loadmodule__+__error__+__control__+__key_Down.py
+++ style-speech-controls/decrease_selected_pitch_or_rate__-__loadmodule__+__error__+__control__+__key_Down.py
@@ -5,6 +5,7 @@ from orca import orca
 from orca.speech import decreaseSpeechPitch, decreaseSpeechRate, decreaseSpeechVolume
 f=open("/tmp/orca-ratechanger", "r")
 _param=f.read()
+f.close()
 if _param == "rate":
 	decreaseSpeechRate()
 elif _param == "pitch":
--- style-speech-controls/increase_selected_pitch_or_rate__-__loadmodule__+__error__+__control__+__key_Up.py
+++ style-speech-controls/increase_selected_pitch_or_rate__-__loadmodule__+__error__+__control__+__key_Up.py
@@ -5,6 +5,7 @@ from orca import orca
 from orca.speech import increaseSpeechPitch, increaseSpeechRate, increaseSpeechVolume
 f = open("/tmp/orca-ratechanger", "r")
 _param=f.read()
+f.close()
 if _param == "rate":
 	increaseSpeechRate()
 elif _param == "pitch":
